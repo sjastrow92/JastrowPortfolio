937500bad44c0b0f95d9005dcf4df6adc4ad0b2f
sjastrow <skjastrow@gmail.com>
2024-06-04 17:42:44 -0400

    Set up initial portfolio layout

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import NavTabs from "./NavTabs";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <NavTabs />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default App;
