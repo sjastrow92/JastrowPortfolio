563ffc0f17dea69cfbb07fab33a63f014b50b809
sjastrow <skjastrow@gmail.com>
2024-07-23 15:32:45 -0400

    added server.js to run cointact form submission.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/send", formData);
+      if (response.status === 200) {
+        alert("Email sent successfully");
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        });
+      }
+    } catch (error) {
+      console.error("Error sending email", error);
+      alert("Failed to send email");
+    }
+  };
+
   return (
     <div style={{ display: "block", width: 700, padding: 30 }}>
       <h4>Contact Me</h4>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>Enter your full name:</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter your full name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
             required
           />
         </Form.Group>
@@ -20,14 +53,25 @@ export default function Contact() {
           <Form.Label>Enter your email address:</Form.Label>
           <Form.Control
             type="email"
-            placeholder="Enter your your email address"
+            placeholder="Enter your email address"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Message:</Form.Label>
-          <Form.Control type="text" placeholder="Message" required />
+          <Form.Control
+            type="text"
+            placeholder="Message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
         </Form.Group>
-        <br></br>
+        <br />
         <Button variant="primary" type="submit">
           Click here to submit form
         </Button>
