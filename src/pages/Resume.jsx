5e10eb3cd5aca102e445097ffe68c31d5cd0e1dc
sjastrow <skjastrow@gmail.com>
2024-11-20 16:22:57 -0500

    updated resume page.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,5 @@
 import ResumePDF from "../assets/StephenJastrow.Resume.pdf";
-import ResumePic from "../assets/resumepic.png";
+import ResumePic from "../assets/resumeupdatedscreenshot.png";
 
 export default function Resume() {
   return (
@@ -8,10 +8,34 @@ export default function Resume() {
       <a href={ResumePDF} download>
         Download
       </a>
-      <div style={{ overflowY: "auto", maxHeight: "calc(100vh - 200px)", textAlign: "center" }}>
-        <img src={ResumePic} alt="Resume" style={{ width: "100%", maxWidth: "800px", height: "auto", maxHeight: "100%" }}/>
+      <div
+        style={{
+          overflowY: "auto",
+          maxHeight: "calc(100vh - 200px)",
+          textAlign: "center",
+        }}
+      >
+        <img
+          src={ResumePic}
+          alt="Resume"
+          style={{
+            width: "100%",
+            maxWidth: "800px",
+            height: "auto",
+            maxHeight: "100%",
+          }}
+        />
       </div>
-      <footer style={{ position: "fixed", bottom: "0", width: "100%", textAlign: "center", padding: "10px", background: "#f1f1f1" }}>
+      <footer
+        style={{
+          position: "fixed",
+          bottom: "0",
+          width: "100%",
+          textAlign: "center",
+          padding: "10px",
+          background: "#f1f1f1",
+        }}
+      >
         © 2024 Your Name. All rights reserved.
       </footer>
     </div>
